6c3d8e1ff94fa77dc9803154bce223eda5531d43
ThalKod <tmarcelin@thal.tech>
2018-06-28 12:14:34 -0400

    add onsubmit action

--- src/App.js
+++ src/App.js
@@ -24,18 +24,30 @@ class App extends Component {
     this.setState(()=>({ manager, players, balance }));
   }
 
+  onSubmit = async (event) =>{
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+    
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, "ether") 
+    });
+
+  };
+
   render() {
     return (
       <div>
         <h2>Lottery Contract</h2>
         <p>
         This contract is managed by: {this.state.manager}
-        There are curently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, "ether")} ether !
+          There are curently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, "ether")} ether !
         </p>
 
         <hr />
 
-        <form action="">
+        <form onSubmit={this.onSubmit}>
             <h4>Wan to try your luck ?</h4>
             <div>
                 <label htmlFor="">Amount of ether to enter</label>
